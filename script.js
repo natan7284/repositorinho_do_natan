c77aa29a5b27946375314205cb368703d7230212
natan7284 <natancamargo282@gmail.com>
2024-09-11 09:51:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
     button.addEventListener('click', function(){
+        console.log("Clicou!")
         const atual = document.querySelector('.ativo');
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
